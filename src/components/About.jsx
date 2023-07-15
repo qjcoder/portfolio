9666babfb1ac892af16e5078c02d0d736dc7d973
qjcoder <qjcoder@gmail.com>
2023-07-15 12:42:51 +0500

    about-section and bug fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "../utils/motion";
+import { services } from "../constants";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card "
+      >
+        <div
+          // eslint-disable-next-line react/no-unknown-property
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain " />
+
+          <h3 className="text-white text-[20px] font-bold text-center ">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] "
+      >
+        I'm skilled MERN Stack Web developer with experience in JavaScript, and
+        frameworks like React, NodeJs, Express and NextJS. I'm quick learner and
+        collaborate closely with clients to create efficient, scalable and
+        user-friendly solution to solve real-world problems. Let's work togather
+        to bring your ideas to life!
+        {/* I have a proven track
+        record of working with the MERN stack, showcasing my expertise in
+        full-stack web development. With a deep understanding of MongoDB,
+        Express.js, React.js, and Node.js,NextJS, I have successfully designed
+        and implemented various web applications.My proficiency in React.js has
+        allowed me to craft engaging and user-friendly interfaces.With a solid
+        foundation in the MERN stack, I am ready to take on new challenges and
+        contribute to the success of future web development projects. */}
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default SectionWrapper(About, "about");
